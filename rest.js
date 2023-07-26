98256ab7537f8866ea2fdab383d840c284be21a8
ramteke3yash <ramteke.yashkun@gmail.com>
2023-07-27 02:21:31 +0530

    newcode added

--- rest.js
+++ rest.js
@@ -1,24 +1,24 @@
 const API_BASE_URL =
-  "https://crudcrud.com/api/d3951cd72e964abb94f856ece9293cbc/restaurant";
+  "https://crudcrud.com/api/a5273de72be8463c89a42fd535d8d15e/rest2";
 
 // Cache frequently used elements
 const form = document.getElementById("my-form");
 const nameInput = document.getElementById("name");
 const descriptionInput = document.getElementById("Description");
 const categoryInput = document.getElementById("Category");
-const expenseList = document.getElementById("expenses");
+const orderList = document.getElementById("orders");
 let editId = null;
 
-// Function to add an expense
-async function addExpense() {
+// Function to add order
+async function addOrder() {
   const name = nameInput.value;
   const description = descriptionInput.value;
   const category = categoryInput.value;
 
-  //Creating a new expense
-  const expense = { name, description, category };
+  //Creating a new order
+  const order = { name, description, category };
   try {
-    await axios.post(API_BASE_URL, expense);
+    await axios.post(API_BASE_URL, order);
   } catch (err) {
     console.log(err);
   }
@@ -29,47 +29,70 @@ async function addExpense() {
   descriptionInput.value = "";
   categoryInput.value = "";
 
-  // Reload and display the expenses
-  loadExpenseList();
+  // Reload and display the orders
+  loadOrderList();
 }
 
-// Function to delete an expense
-async function deleteExpense(id) {
+// Function to delete an order
+async function deleteOrder(id) {
   try {
     await axios.delete(`${API_BASE_URL}/${id}`);
   } catch (err) {
     console.log(err);
   }
 
-  // Reload and display the expenses
-  loadExpenseList();
+  // Reload and display the orders
+  loadOrderList();
 }
 
-// Function to load existing expenses from the API and display them
-async function loadExpenseList() {
+async function loadOrderList() {
   try {
     const response = await axios.get(API_BASE_URL);
-    const expenses = response.data;
+    const orders = response.data;
 
-    // Clear the expense list
-    expenseList.innerHTML = "";
+    // Get the order div container
+    const orderContainer = document.getElementById("orders");
+    orderContainer.innerHTML = ""; // Clear previous content
 
-    // Display each expense in the expense list
-    expenses.forEach(function (expense, index) {
-      const li = document.createElement("li");
-      li.textContent = `₹${expense.name},${expense.description},${expense.category}`;
+    // Group orders by category using reduce
+    const ordersByCategory = orders.reduce((acc, order) => {
+      const { category } = order;
+      if (!acc[category]) {
+        acc[category] = [];
+      }
+      acc[category].push(order);
+      return acc;
+    }, {});
 
-      // Create the Delete button
-      const deleteButton = document.createElement("button");
-      deleteButton.textContent = "Delete order";
-      deleteButton.addEventListener("click", function () {
-        deleteExpense(expense._id);
+    // Display each category with its orders
+    for (const category in ordersByCategory) {
+      const categoryDiv = document.createElement("div");
+
+      // Create a heading for the category
+      const categoryHeading = document.createElement("h2");
+      categoryHeading.textContent = `${category}`;
+      categoryDiv.appendChild(categoryHeading);
+
+      // Add each order item under the category
+      ordersByCategory[category].forEach(function (order, index) {
+        const orderDiv = document.createElement("div");
+        orderDiv.textContent = `₹${order.name}, ${order.description}`;
+
+        // Create the Delete button
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete order";
+        deleteButton.addEventListener("click", function () {
+          deleteOrder(order._id);
+        });
+        orderDiv.appendChild(deleteButton);
+
+        // Append order item to the categoryDiv
+        categoryDiv.appendChild(orderDiv);
       });
-      li.appendChild(deleteButton);
 
-      // Append list item to the expense list
-      expenseList.appendChild(li);
-    });
+      // Append the categoryDiv to the orderContainer
+      orderContainer.appendChild(categoryDiv);
+    }
   } catch (err) {
     console.log(err);
   }
@@ -78,8 +101,8 @@ async function loadExpenseList() {
 // Add event listener for form submission
 form.addEventListener("submit", function (e) {
   e.preventDefault(); // Prevent form submission
-  addExpense();
+  addOrder();
 });
 
-// Load existing expenses from the API and display them
-document.addEventListener("DOMContentLoaded", loadExpenseList);
+// Load existing orders from the API and display them
+document.addEventListener("DOMContentLoaded", loadOrderList);
